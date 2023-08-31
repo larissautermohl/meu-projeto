32415fdf4f3640387acb5a1b2d08be09f5365f4c
Larissa Utermohl <larissautermohl@gmail.com>
2023-08-31 19:24:31 -0300

    Explicação Type

--- atividade2.py
+++ atividade2.py
@@ -1,1 +1,7 @@
 print (145 + 234)
+
+nome = "Lara" 
+idade = 20
+
+print("Ola meu nome" + nome)
+print ("Ola meu nome é " + nome + "e tenho" + idade )
---
+++ atividade3.py
@@ -0,0 +1,12 @@
+'''Leiam o caso abaixo e executem usando Python.
+
+A loja "ROUPAS SA" tem 2000 clientes e quer enviar mensagens nominais a cada um. A mensagem seria a seguinte:
+
+Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00 e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10.'''
+nome = "PAULA MARTINS"
+mes = "JANEIRO"
+valor = "500,00"
+desconto = "10"
+cupom = "PAULAÉ10"
+
+print("Olá," + nome +  ". Em"  +  mes  + "você realizou uma compra no valor de R$" + valor + "e ganhou um desconto de" + desconto + " % em sua próxima compra. Use o cupom" + cupom + "")
