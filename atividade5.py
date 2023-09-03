0051c041a0d589e474c8bb2497ab8a3648062c89
Larissa Utermohl <larissautermohl@gmail.com>
2023-09-03 19:52:22 -0300

    Featured: adicionar calculo e notas

---
+++ atividade5.py
@@ -0,0 +1,12 @@
+# Capturando os dados do usuário
+nome = input("Digite o seu nome: ")
+altura = float(input("Digite a sua altura em metros: "))
+idade = int(input("Digite a sua idade: "))
+
+# Imprimindo os dados na tela
+'''print("Dados do usuário:")
+print("Nome:", nome)
+print("Altura:", altura, "metros")
+print("Idade:", idade, "anos")'''
+
+print(f" O nome é Nome: {nome} Altura: {altura:} metros Idade: {idade} anos")
